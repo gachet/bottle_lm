e2945406e245dec1c86a32b6865d6f12b272587f
José Domingo Muñoz <pledin.jd@gmail.com>
2015-02-26 10:24:57 +0100

    Tercer ejercicio

---
+++ 3_formularios/programa.py
@@ -0,0 +1,21 @@
+from bottle import Bottle,route,run,request
+@route('/login') 
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>'''
+
+@route('/login',method='POST') 
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if username=="pepe" and password=="asdasd":
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>Login failed.</p>"
+
+run(host='localhost', port=8080)
+
