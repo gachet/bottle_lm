e34f56104f73c548b7a0f6b2272ed7c4592bca25
josedom24 <josedom24@gmail.com>
2015-03-07 23:07:25 +0100

    Templates

--- 4_plantillas/programa.py
+++ 4_plantillas/programa.py
@@ -6,7 +6,7 @@ def hello(name='Mundo'):
     return template('template_hello.tpl', nombre=name)
 @route('/suma/<num1>/<num2>')
 def suma(num1,num2):
-    return template('template_suma.tpl',numero1=num1,numero=num2)
-    
+    return template('template_suma.tpl',numero1=num1,numero2=num2)
+
 run(host='0.0.0.0', port=8080)
 
