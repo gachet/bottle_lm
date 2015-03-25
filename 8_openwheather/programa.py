b336e89e29908857294e038c511e3b4080bd74f6
Jose Domingo <jose@debian.gonzalonazareno.org>
2015-03-25 10:45:59 +0100

    8_openwheather

--- 8_openwheather/programa.py
+++ 8_openwheather/programa.py
@@ -1,21 +1,20 @@
 from bottle import Bottle,route,run,request
-@route('/login') 
+import requests
+@route('/') 
 def login():
     return '''
-        <form action="/login" method="post">
-            Username: <input name="username" type="text" />
-            Password: <input name="password" type="password" />
+        <form action="/tiempo" method="post">
+            Ciudad: <input name="ciudad" type="text" />
+            <br/>
             <input value="Login" type="submit" />
         </form>'''
 
-@route('/login',method='POST') 
-def do_login():
-    username = request.forms.get('username')
-    password = request.forms.get('password')
-    if username=="pepe" and password=="asdasd":
-        return "<p>Your login information was correct.</p>"
-    else:
-        return "<p>Login failed.</p>"
+@route('/tiempo',method='POST') 
+def calcula_tiempo():
+    ciudad = request.forms.get('ciudad')
+    r=requests.get('api.openweathermap.org/data/2.5/weather?q=%s&mode=xml&units=metric'%ciudad)
+    if r.status_code = 200:
+        return r.text
 
 run(host='localhost', port=8080)
 
